c49beff67c99ab20c62dd84d55ad231778cd6d17
Haider540 <91596648+Haider540@users.noreply.github.com>
2022-02-17 20:32:50 +0000

    ih
    huh

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -6,9 +6,53 @@ namespace ConsoleAppProject.App01
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Haider Imam
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+        public void Run()
+        {
+            OutputHeading();
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+
+        private void OutputHeading()
+        {
+            Console.WriteLine("\n-------------------------------");
+            Console.WriteLine("      covert miles to feet       ");
+            Console.WriteLine("         by Haider Imam          ");
+            Console.WriteLine("-------------------------------\n");
+
+        }
+
+        /// <summary>
+        /// prompt the user to enter the distance in miles
+        /// Input the miles as a two digit number
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.Write("please enter the number of miles > ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CalculateFeet()
+        {
+            feet = miles * 5280;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OutputFeet()
+        {
+            Console.WriteLine(miles + " miles is " + feet + "feet!");
+        }
+
     }
 }
