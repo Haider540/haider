25e2bd9c14e6f83b25a5fc3b032b00caec18084c
Haider540 <91596648+Haider540@users.noreply.github.com>
2022-04-06 13:25:06 +0100

    app04

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleAppProject.App01;
 using ConsoleAppProject.App02;
 using ConsoleAppProject.App03;
+using ConsoleAppProject.App04;
 using ConsoleAppProject.Helpers;
 using System;
 
@@ -28,35 +29,44 @@ namespace ConsoleAppProject
             Console.WriteLine();
             Console.Beep();
 
-            SelectApp();
+            Menu();
 
         }
 
         // User selects the app which they want to use 
-        private static void SelectApp()
+        private static void Menu()
         {
             Console.WriteLine("Which app do you want to select? : ");
-            string[] choices = { "Distance Converter", "BMI Calculator", "Student Marks" };
-            int choice = ConsoleHelper.SelectChoice(choices);
+            string[] choices = { "Distance Converter", "BMI Calculator", "Social Network", "Student Marks" };
+            string choice = Console.ReadLine();
 
-       //first app
-            if (choice == 1)
+            //first app
+            if (choice == "1")
             {
                 DistanceConverter converter = new DistanceConverter();
                 converter.ConverterDistance();
             }
-       //second app02
-            else if (choice == 2)
+            else if (choice == "2")
             {
                 BMI bmi = new BMI();
                 bmi.Run();
             }
-       //third app03
-            else
+            else if (choice == "3")
             {
                 StudentGrades grades = new StudentGrades();
                 grades.Run();
             }
+            else if (choice == "4")
+            {
+                SocialNetwork social = new SocialNetwork();
+                social.DisplayMenu();
+            }
+            else
+            {
+                Console.WriteLine("1 2 or 3 mmmmmmmmm");
+            }
+
+
         }
     }
 }
